388631aec6a1a26a4009088d8d0643f9f250a275
Conan Mercer <conan.mercer1@gmail.com>
2023-07-14 13:18:41 +0200

    update, config file. add, PIR code to test setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,36 @@
 #include <Arduino.h>
 
+const int PIR_SENSOR_OUTPUT_PIN = 26; /* PIR sensor O/P pin */
+int warm_up;
+
 void setup()
 {
-  Serial.begin(9600);
+  pinMode(PIR_SENSOR_OUTPUT_PIN, INPUT);
+  Serial.begin(115200); // initialize serial
+  Serial.println("Waiting For Power On Warm Up");
+  delay(20000); /* Power On Warm Up Delay */
+  Serial.println("Ready!");
 }
 
 void loop()
 {
-  Serial.println("Hello world!");
-  delay(1000);
+  int sensor_output;
+  sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN);
+  if (sensor_output == LOW)
+  {
+    if (warm_up == 1)
+    {
+      Serial.print("Warming Up\n\n");
+      warm_up = 0;
+      delay(2000);
+    }
+    Serial.print("No object in sight\n\n");
+    delay(1000);
+  }
+  else
+  {
+    Serial.print("Object detected\n\n");
+    warm_up = 1;
+    delay(1000);
+  }
 }
