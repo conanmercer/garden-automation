da3fea833405db95c36c7151b2f8fa5e6c5687ca
ConanMercer <conan.mercer1@gmail.com>
2023-09-03 17:23:29 +0200

    update, code to work with new setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
 
-const int PIR_SENSOR_OUTPUT_PIN_1 = 26;
-const int PIR_SENSOR_OUTPUT_PIN_2 = 27;
-const int PIR_SENSOR_OUTPUT_PIN_3 = 25;
+// Right PIR Sensor
+const int PIR_SENSOR_OUTPUT_PIN_1 = 25;
+// Middle PIR Sensor
+const int PIR_SENSOR_OUTPUT_PIN_2 = 26;
+// Left PIR Sensor
+const int PIR_SENSOR_OUTPUT_PIN_3 = 27;
 
 /* Solenoid valve connected to GPIO pin 4 connected to Relay pin IN_1 */
 /* Solenoid valve connected to GPIO pin 5 connected to Relay pin IN_2 */
 /* Solenoid valve connected to GPIO pin 13 connected to Relay pin IN_3 */
 /* Solenoid valve connected to GPIO pin 14 connected to Relay pin IN_4 */
 /* Solenoid valve connected to GPIO pin 16 connected to Relay pin IN_5 */
+
+// Side Garden Water = pin 1
+// Terrace Water = pin 2
+// Right Sprinkler Water = pin 3
+// Middle Sprinkler = pin 4
 
 const int SOLENOID_VALVE_OUTPUT_PINS[] = {4, 5, 13, 14, 16};
 const int NUM_SOLENOID_VALVES = sizeof(SOLENOID_VALVE_OUTPUT_PINS) / sizeof(SOLENOID_VALVE_OUTPUT_PINS[0]);
@@ -25,10 +33,8 @@ void setup()
     pinMode(SOLENOID_VALVE_OUTPUT_PINS[i], OUTPUT);
   }
 
-  // Set the PIR sensor pins as inputs with pull-up resistors
   // When the PIR senses activity in it's viewing area
-  // it pulls the alarm pin low. But when the sensor is inactive, the pin is basically floating.
-  // To avoid any false-positives, the alarm output should be pulled high to 5V
+  // it pulls the alarm pin HIGH. But when the sensor is inactive, the pin is basically floating.
   pinMode(PIR_SENSOR_OUTPUT_PIN_1, INPUT);
   pinMode(PIR_SENSOR_OUTPUT_PIN_2, INPUT);
   pinMode(PIR_SENSOR_OUTPUT_PIN_3, INPUT);
@@ -80,8 +86,9 @@ void turnOnRightSprinkler()
 
   delay(100);
 
-  turnOnSolenoidValve(0);
   turnOnSolenoidValve(3);
+  delay(500);
+  turnOnSolenoidValve(0);
   delay(10000);
   turnOffSolenoidValve(3);
 }
@@ -90,50 +97,58 @@ void turnOnMiddleSprinkler()
 {
   turnOffAllSolenoidValves();
 
-  delay(100);
-
-  turnOnSolenoidValve(0);
   turnOnSolenoidValve(4);
+  delay(500);
+  turnOnSolenoidValve(0);
   delay(10000);
   turnOffSolenoidValve(4);
 }
 
 void loop()
 {
-  int sensor_output_1 = digitalRead(PIR_SENSOR_OUTPUT_PIN_1);
-  int sensor_output_2 = digitalRead(PIR_SENSOR_OUTPUT_PIN_2);
 
-  int sensor_output_3 = digitalRead(PIR_SENSOR_OUTPUT_PIN_3);
+  int left_sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN_3);
+  int middle_sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN_2);
+  int right_sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN_1);
 
-  if (sensor_output_1 == LOW || sensor_output_2 == LOW)
-  {
-    if (warm_up == 1)
-    {
-      warm_up = 0;
-      delay(2000);
-    }
-    delay(1000);
-  }
-  else
-  {
+  if (right_sensor_output == HIGH)
     turnOnRightSprinkler();
-    warm_up = 1;
-    delay(1000);
-  }
 
-  // if (sensor_output_3 == LOW)
+  if (middle_sensor_output == HIGH)
+    turnOnRightSprinkler();
+
+  if (left_sensor_output == HIGH)
+    turnOnMiddleSprinkler();
+
+  // if (right_sensor_output == LOW)
   // {
   //   if (warm_up == 1)
   //   {
   //     warm_up = 0;
   //     delay(2000);
   //   }
-  //   delay(1000);
+  //   delay(100);
   // }
   // else
   // {
-  //   turnOnMiddleSprinkler();
+  //   turnOnRightSprinkler();
   //   warm_up = 1;
-  //   delay(1000);
+  //   delay(100);
+  // }
+
+  // if (middle_sensor_output == LOW)
+  // {
+  //   if (warm_up == 1)
+  //   {
+  //     warm_up = 0;
+  //     delay(2000);
+  //   }
+  //   delay(100);
+  // }
+  // else
+  // {
+  //   turnOnRightSprinkler();
+  //   warm_up = 1;
+  //   delay(100);
   // }
 }
