140ee41eb5162f72188705843258eea46073c4b9
ConanMercer <conan.mercer1@gmail.com>
2024-03-10 16:30:25 +0100

    update, constants header file

--- src/constants.h
+++ src/constants.h
@@ -1,3 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
 // Solenoid Valve Pins
 const int SOLENOID_VALVE_PINS[] = {4, 5, 13, 14, 16};
 const int NUM_SOLENOID_VALVES = sizeof(SOLENOID_VALVE_PINS) / sizeof(SOLENOID_VALVE_PINS[0]);
+
+#endif // CONSTANTS_H
