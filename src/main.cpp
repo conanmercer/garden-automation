31d2806a5e2a709d1132be31cab841df3d7ae84c
ConanMercer <conan.mercer1@gmail.com>
2024-04-12 16:28:29 +0200

    update, summer time and motion sensor

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,12 @@ unsigned long lastMotionTime = 0; // Timestamp of the last motion detected
 int motionCount = 0;              // Counter for motion detections
 bool motionCountExceeded = false; // Flag to track if motion count exceeded
 
-// const unsigned long irrigationInterval = 2UL * 24 * 60 * 60 * 1000; // 2 days in milliseconds (summer)
-// const unsigned long sprinklersInterval = 24 * 60 * 60 * 1000;       // 1 day in milliseconds (summer)
-const unsigned long irrigationInterval = 6UL * 24 * 60 * 60 * 1000; // 6 days in milliseconds (winter)
-const unsigned long sprinklersInterval = 7UL * 24 * 60 * 60 * 1000; // 7 days in milliseconds (winter)
-// const unsigned long irrigationInterval = 12UL * 60 * 1000; // 12 minutes in milliseconds (for testing)
-// const unsigned long sprinklersInterval = 14UL * 60 * 1000; // 14 minutes in milliseconds (for testing)
+const unsigned long irrigationInterval = 2UL * 24 * 60 * 60 * 1000; // 2 days in milliseconds (summer)
+const unsigned long sprinklersInterval = 24 * 60 * 60 * 1000;       // 1 day in milliseconds (summer)
+// const unsigned long irrigationInterval = 6UL * 24 * 60 * 60 * 1000; // 6 days in milliseconds (winter)
+// const unsigned long sprinklersInterval = 7UL * 24 * 60 * 60 * 1000; // 7 days in milliseconds (winter)
+// const unsigned long irrigationInterval = 25UL * 60 * 1000; // 12 minutes in milliseconds (for testing)
+// const unsigned long sprinklersInterval = 27UL * 60 * 1000; // 14 minutes in milliseconds (for testing)
 
 Scheduler scheduler(irrigationInterval, sprinklersInterval);
 PinInitializer pinInitializer;
@@ -46,24 +46,24 @@ void loop()
 
   // Check PIR sensor pins for motion
   // Check if motionCountExceeded is false before running the loop
-  // if (!motionCountExceeded)
-  // {
-  //   // Check PIR sensor pins for motion
-  //   for (int i = 0; i < 3; i++)
-  //   {
-  //     if (digitalRead(PIR_SENSOR_PINS[i]) == HIGH)
-  //     {
-  //       motionDetected = true;
-  //       motionCount++;
+  if (!motionCountExceeded)
+  {
+    // Check PIR sensor pins for motion
+    for (int i = 0; i < 3; i++)
+    {
+      if (digitalRead(PIR_SENSOR_PINS[i]) == HIGH)
+      {
+        motionDetected = true;
+        motionCount++;
 
-  //       if (!motionCountExceeded && motionCount > 5)
-  //       {
-  //         motionCountExceeded = true;
-  //         lastMotionTime = currentTime;
-  //       }
-  //     }
-  //   }
-  // }
+        if (!motionCountExceeded && motionCount > 5)
+        {
+          motionCountExceeded = true;
+          lastMotionTime = currentTime;
+        }
+      }
+    }
+  }
 
   // Process motion detection
   if (motionDetected)
