3aa36280f6ae482b66fa94ae49fae818d33d7e87
ConanMercer <conan.mercer1@gmail.com>
2024-07-15 12:40:13 +0200

    add, more solenoids

--- src/constants.h
+++ src/constants.h
@@ -2,7 +2,7 @@
 #define CONSTANTS_H
 
 // Solenoid Valve Pins
-const int SOLENOID_VALVE_PINS[] = {4, 5, 13, 14, 16, 18};
+const int SOLENOID_VALVE_PINS[] = {4, 5, 13, 14, 16, 18, 34, 12, 15};
 const int NUM_SOLENOID_VALVES = sizeof(SOLENOID_VALVE_PINS) / sizeof(SOLENOID_VALVE_PINS[0]);
 // PIR Sensor Pins
 const int PIR_SENSOR_PINS[] = {27, 26, 25}; // Left, Middle, Right
