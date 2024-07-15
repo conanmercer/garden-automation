213b081961cbc9db35cd01ced7ae0a0cc9e7dff3
ConanMercer <conan.mercer1@gmail.com>
2024-07-15 12:41:13 +0200

    add, new sprinkler and irrigation lines

--- src/irrigation.cpp
+++ src/irrigation.cpp
@@ -11,8 +11,15 @@ void runIrrigation()
     turnOffAllSolenoidValves();
     turnOnSolenoidValve(0);
     turnOnSolenoidValve(1);
-    unsigned long sideGardenDelay = random(13, 15) * 60 * 1000; // Generate random delay between 5 and 10 minutes
-    delay(sideGardenDelay);                                     // Side garden random between 5 and 10 minutes
+    unsigned long gardenDelay = random(13, 15) * 60 * 1000; // Generate random delay between 13 and 15 minutes
+    delay(gardenDelay);                                     // Side Garden lower
+    // turnOffAllSolenoidValves();
+    // turnOnSolenoidValve(0);
+    // turnOnSolenoidValve(6);
+    // delay(gardenDelay); // Side Garden
+    turnOnSolenoidValve(0);
+    turnOnSolenoidValve(7);
+    delay(gardenDelay); // Top Garden
     turnOffAllSolenoidValves();
 }
 
@@ -21,15 +28,19 @@ void runSprinklers()
     turnOffAllSolenoidValves();
     turnOnSolenoidValve(0);
     turnOnSolenoidValve(3);
-    unsigned long sprinklerDelay = random(13, 15) * 60 * 1000; // Generate random delay between 5 and 10 minutes
-    delay(sprinklerDelay);                                     // Right Sprinkler random between 5 and 10 minutes
+    unsigned long sprinklerDelay = random(13, 15) * 60 * 1000; // Generate random delay between 13 and 15 minutes
+    delay(sprinklerDelay);                                     // Right Sprinkler
+    turnOffAllSolenoidValves();
+    turnOnSolenoidValve(0);
+    turnOnSolenoidValve(8);
+    delay(sprinklerDelay); // Back Right Sprinkler
     turnOffAllSolenoidValves();
     turnOnSolenoidValve(0);
     turnOnSolenoidValve(4);
-    delay(sprinklerDelay); // Middle Sprinkler random between 9 and 11 minutes
+    delay(sprinklerDelay); // Middle Sprinkler
     turnOffAllSolenoidValves();
     turnOnSolenoidValve(0);
     turnOnSolenoidValve(5);
-    delay(sprinklerDelay); // Left Sprinkler random between 9 and 11 minutes
+    delay(sprinklerDelay); // Left Sprinkler
     turnOffAllSolenoidValves();
 }
